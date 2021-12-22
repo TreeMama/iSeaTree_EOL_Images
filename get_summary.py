416f63c11f5a8b50d097a7a4708944763a8cb368
Shreya Banga <shreya.banga@gmail.com>
2021-12-22 18:04:24 -0500

    Species 1 images + decriptions
    All done except images for ids 6,9,10,13,18,20,24,25,31,43

--- get_summary.py
+++ get_summary.py
@@ -3,31 +3,32 @@ import wikipedia
 import json
 import os
 
-parent_dir = "/Users/shreyabanga/Desktop/iSeaTree_EOL_Images/missing_images/226/info.json"
+parent_dir = "/Users/shreyabanga/Desktop/iSeaTree_EOL_Images/missing_images/"
 
-f = open('species3.json')
+f = open('species1.json')
 data = json.load(f)
 
-# for i in data:
+for i in data:
 
-#     # if i['FULL_PIC'] == "":
+    if i['FULL_PIC'] == "":
 
-#     # print(i['SCIENTIFIC']+'\n')
-#     try:
-#         directory = i['ID'] + '/info.json'
-#         path = os.path.join(parent_dir,directory)
-#         # os.mkdir(path)
-#         g = open(path, 'w')
-#         g.write("{\n[\n\"URL\": \"\",\n]\n}")
-#         g.close()
-#             # os.mkdir(path)
-#             # print(wikipedia.summary(i['SCIENTIFIC'].replace(" ","_"))+'\n\n')
-#     except:
-#         print("fail")
-g = open(parent_dir)
-dataa = json.load(g)
-print(dataa)
-g.close()
+        
+        try:
+            directory = i['ID'] + "/wikimedia_commons"#+ '/info.json'
+            path = os.path.join(parent_dir,directory)
+            os.mkdir(path)
+            # g = open(path, 'w')
+            # g.write("{\n[\n\"URL\": \"\",\n]\n}")
+            # g.close()
+                # os.mkdir(path)
+            # print(wikipedia.summary(i['SCIENTIFIC'].replace(" ","_"))+'\n\n')
+        except:
+            # continue
+            print("fail")
+# g = open(parent_dir)
+# dataa = json.load(g)
+# print(dataa)
+# g.close()
 f.close()
 
 # finding result for the search
