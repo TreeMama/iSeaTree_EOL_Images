9605553be565ef6cb76aef6fa283e23aa929ea63
Christy <christyheaton@gmail.com>
2022-08-23 15:14:36 -0700

    Fixed spacing in comments

--- missing_genus_finder.py
+++ missing_genus_finder.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     data = json.load(open(args.jsonfile, 'r'))
 
-	# create empty lists to hold genera
+    # create empty lists to hold genera
     genusList = []
     sppList = []
 
@@ -17,16 +17,16 @@ if __name__ == '__main__':
         commonName = tree['COMMON']
         genus = tree['GENUS']
 		
-		# append genus if it is not already in the list
+	# append genus if it is not already in the list
         if genus not in genusList:
             genusList.append(genus)
 			
-		# append genus if it is already represented as a species (i.e. contains 'spp' in the common name)
+	# append genus if it is already represented as a species (i.e. contains 'spp' in the common name)
         if 'spp' in commonName:
             sppList.append(genus)
 	
-	# create a new list of unique genera, removing those that are already accounted for
+    # create a new list of unique genera, removing those that are already accounted for
     missingGenusList = [x for x in genusList if x not in sppList]
 	
-	# print result
+    # print result
     print(missingGenusList)
